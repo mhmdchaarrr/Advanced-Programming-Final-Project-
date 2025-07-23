c8f6f1625b309a3b8c4434614420c1e6f92de686
FidyanNagaria <96497152+FidyanNagaria@users.noreply.github.com>
2025-07-23 23:51:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,34 @@
 import os
 from contact import Contact
 from contactbook import ExtendedContactBook
-from decorators import Colors
+
+
+#Decorators for changing color of text
+class Colors:
+    BLUE = '\033[94m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+
+def deco(color: str):
+    colors = {
+        "red": "\033[91m",
+        "green": "\033[92m",
+        "blue": "\033[94m",
+        "yellow": "\033[93m",
+    }
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print(colors.get(color, ""), end="")
+            result = func(*args, **kwargs)
+            print("\033[0m", end="")
+            return result
+        return wrapper
+    return decorator
+
+
 
 CONFIG_FILE = "user_config.txt"
 name = ""
